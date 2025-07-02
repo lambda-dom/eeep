fbca2c0cd8d6cffd8c0b4e4089e7ca4a643f79a4
lambda-dom <grodrigues.math@gmail.com>
2025-07-02 16:16:58 +0100

    Resref (temp) type.

---
+++ src/Eeep/Types/Opcode/Resref.hs
@@ -0,0 +1,20 @@
+{- |
+Module: Eeep.Types.Opcode.Resref
+
+The @Resref@ type.
+-}
+
+module Eeep.Types.Opcode.Resref (
+    -- * Types.
+    Resref (..),
+) where
+
+-- Imports.
+-- Base.
+import Data.Ix (Ix)
+import Data.Word (Word64)
+
+
+{- | The 'Resref' type for resource references. -}
+newtype Resref = Resref Word64
+    deriving stock (Eq, Ord, Ix)
