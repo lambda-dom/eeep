dc6bb03f3292ecf2b4bc048c4d7896d6b30c3f05
lambda-dom <grodrigues.math@gmail.com>
2025-07-07 12:23:43 +0100

    Refactor `Effect` parser.

--- src/Eeep/Types/Effect/Projectile.hs
+++ src/Eeep/Types/Effect/Projectile.hs
@@ -7,13 +7,33 @@ The @Projectile@ type.
 module Eeep.Types.Effect.Projectile (
     -- * Types.
     Projectile (..),
+
+    -- * Parsers.
+    parseProjectile,
 ) where
 
 -- Imports.
 -- Base.
-import Data.Word (Word32)
+import Data.Word (Word8, Word32)
+
+-- non-Hackage libraries.
+import Mono.Typeclasses.MonoFunctor (ElementOf)
+import Mono.Typeclasses.MonoFoldable (MonoFoldable)
+import Trisagion.Typeclasses.HasOffset (HasOffset)
+import Trisagion.Typeclasses.Splittable (Splittable (PrefixOf))
+import Trisagion.Parser (Parser)
+import Trisagion.Parsers.ParseError (capture)
+import Trisagion.Parsers.Streamable (InputError)
+import Trisagion.Parsers.Word8 (word32Le)
 
 
 {- | The t'Projectile' numeric id type. -}
 newtype Projectile = Projectile Word32
     deriving stock (Eq, Show)
+
+
+{- | Parse a t'Projectile' numeric id. -}
+parseProjectile
+    :: (HasOffset s, Splittable s, MonoFoldable (PrefixOf s), ElementOf (PrefixOf s) ~ Word8)
+    => Parser s InputError Projectile
+parseProjectile = capture . fmap Projectile $ word32Le
--- src/Eeep/Types/Effect/School.hs
+++ src/Eeep/Types/Effect/School.hs
@@ -7,13 +7,33 @@ The @School@ type.
 module Eeep.Types.Effect.School (
     -- * Types.
     School (..),
+
+    -- * Parsers.
+    parseSchool,
 ) where
 
 -- Imports.
 -- Base.
-import Data.Word (Word32)
+import Data.Word (Word8, Word32)
+
+-- non-Hackage libraries.
+import Mono.Typeclasses.MonoFunctor (ElementOf)
+import Mono.Typeclasses.MonoFoldable (MonoFoldable)
+import Trisagion.Typeclasses.HasOffset (HasOffset)
+import Trisagion.Typeclasses.Splittable (Splittable (PrefixOf))
+import Trisagion.Parser (Parser)
+import Trisagion.Parsers.ParseError (capture)
+import Trisagion.Parsers.Streamable (InputError)
+import Trisagion.Parsers.Word8 (word32Le)
 
 
 {- | The magic t'School' numeric id type. -}
 newtype School = School Word32
     deriving stock (Eq, Show)
+
+
+{- | Parse a t'School' numeric id. -}
+parseSchool
+    :: (HasOffset s, Splittable s, MonoFoldable (PrefixOf s), ElementOf (PrefixOf s) ~ Word8)
+    => Parser s InputError School
+parseSchool = capture . fmap School $ word32Le
