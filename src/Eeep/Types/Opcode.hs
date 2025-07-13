51fd37944a08226b032562208448dd2d2d65f332
lambda-dom <grodrigues.math@gmail.com>
2025-07-14 00:28:56 +0100

    `Opcode` serializer.

--- src/Eeep/Types/Opcode.hs
+++ src/Eeep/Types/Opcode.hs
@@ -13,7 +13,10 @@ module Eeep.Types.Opcode (
     Opcode (..),
 
     -- * Parsers.
-    parseOpcode,
+    decodeOpcode,
+
+    -- * Serializers.
+    encodeOpcode,
 ) where
 
 -- Imports.
@@ -27,24 +30,27 @@ import Mono.Typeclasses.MonoFoldable (MonoFoldable)
 import Trisagion.Types.ParseError (ParseError)
 import Trisagion.Typeclasses.HasOffset (HasOffset)
 import Trisagion.Typeclasses.Splittable (Splittable (PrefixOf))
+import Trisagion.Typeclasses.Binary (Binary)
 import Trisagion.Parser (Parser)
 import Trisagion.Parsers.ParseError (capture, onParseError)
 
 -- Package.
-import Eeep.Types.Opcode.OpType (OpType, decodeOpType16)
-import Eeep.Types.Opcode.Power (Power, decodePower8)
-import Eeep.Types.Opcode.Target (Target, decodeTarget8)
-import Eeep.Types.Opcode.Parameter (Parameter, decodeParameter)
-import Eeep.Types.Opcode.Timing (Timing, decodeTiming8)
-import Eeep.Types.Opcode.Duration (Duration, decodeDuration)
-import Eeep.Types.Opcode.ResistDispel (ResistDispel, decodeResistDispel8)
-import Eeep.Types.Opcode.Probability (Probability, decodeProbability8)
-import Eeep.Types.Opcode.Resref (Resref, decodeResref)
-import Eeep.Types.Opcode.DiceNumber (DiceNumber, decodeDiceNumber)
-import Eeep.Types.Opcode.DiceSides (DiceSides, decodeDiceSides)
-import Eeep.Types.Opcode.SaveFlags (SaveFlags, decodeSaveFlags)
-import Eeep.Types.Opcode.SaveBonus (SaveBonus, decodeSaveBonus)
-import Eeep.Types.Opcode.Special (Special, decodeSpecial)
+import Eeep.Types.Opcode.OpType (OpType, decodeOpType16, encodeOpType16)
+import Eeep.Types.Opcode.Power (Power, decodePower8, encodePower8)
+import Eeep.Types.Opcode.Target (Target, decodeTarget8, encodeTarget8)
+import Eeep.Types.Opcode.Parameter (Parameter, decodeParameter, encodeParameter)
+import Eeep.Types.Opcode.Timing (Timing, decodeTiming8, encodeTiming8)
+import Eeep.Types.Opcode.Duration (Duration, decodeDuration, encodeDuration)
+import Eeep.Types.Opcode.ResistDispel (ResistDispel, decodeResistDispel8, encodeResistDispel8)
+import Eeep.Types.Opcode.Probability (Probability, decodeProbability8, encodeProbability8)
+import Eeep.Types.Opcode.Resref (Resref, decodeResref, encodeResref)
+import Eeep.Types.Opcode.DiceNumber (DiceNumber, decodeDiceNumber, encodeDiceNumber)
+import Eeep.Types.Opcode.DiceSides (DiceSides, decodeDiceSides, encodeDiceSides)
+import Eeep.Types.Opcode.SaveFlags (SaveFlags, decodeSaveFlags, encodeSaveFlags)
+import Eeep.Types.Opcode.SaveBonus (SaveBonus, decodeSaveBonus, encodeSaveBonus)
+import Eeep.Types.Opcode.Special (Special, decodeSpecial, encodeSpecial)
+import Trisagion.Serializer (Serializer)
+import Data.Functor.Contravariant (Contravariant(..))
 
 
 {- | The t'OpcodeError' type. -}
@@ -72,11 +78,11 @@ data Opcode = Opcode {
     } deriving stock (Eq, Show)
 
 
-{- | Parse an t'Effect'. -}
-parseOpcode
+{- | Decode an t'Ocode'. -}
+decodeOpcode
     :: forall s . (HasOffset s, Splittable s, MonoFoldable (PrefixOf s), ElementOf s ~ Word8, ElementOf (PrefixOf s) ~ Word8)
     => Parser s (ParseError OpcodeError) Opcode
-parseOpcode = capture $ do
+decodeOpcode = capture $ do
         optype      <- onError decodeOpType16
         target      <- onError decodeTarget8
         power       <- onError decodePower8
@@ -96,3 +102,23 @@ parseOpcode = capture $ do
     where
         onError :: (Typeable e, Eq e, Show e) => Parser s (ParseError e) a -> Parser s (ParseError OpcodeError) a
         onError = onParseError OpcodeError
+
+
+{- | Encode an t'Opcode'. -}
+encodeOpcode :: Binary m => Serializer m Opcode
+encodeOpcode
+    =  contramap optype encodeOpType16
+    <> contramap target encodeTarget8
+    <> contramap power encodePower8
+    <> contramap parameter1 encodeParameter
+    <> contramap parameter2 encodeParameter
+    <> contramap timing encodeTiming8
+    <> contramap dispel encodeResistDispel8
+    <> contramap duration encodeDuration
+    <> contramap probability encodeProbability8
+    <> contramap resource encodeResref
+    <> contramap dicenumber encodeDiceNumber
+    <> contramap dicesides encodeDiceSides
+    <> contramap saveflags encodeSaveFlags
+    <> contramap savebonus encodeSaveBonus
+    <> contramap special encodeSpecial
