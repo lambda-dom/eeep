5805b60c744bd2c349ba0e33ac3b1edd15427d07
lambda-dom <grodrigues.math@gmail.com>
2025-07-15 18:44:08 +0100

    `Effect` fields use serializers directly.

--- src/Eeep/Types/Effect/Sectype.hs
+++ src/Eeep/Types/Effect/Sectype.hs
@@ -26,12 +26,12 @@ import Mono.Typeclasses.MonoFoldable (MonoFoldable)
 import Trisagion.Typeclasses.HasOffset (HasOffset)
 import Trisagion.Typeclasses.Splittable (Splittable (PrefixOf))
 import Trisagion.Typeclasses.Binary (Binary)
-import qualified Trisagion.Typeclasses.Binary as Binary (word32Le)
 import Trisagion.Parser (Parser)
 import Trisagion.Parsers.ParseError (capture)
 import Trisagion.Parsers.Streamable (InputError)
 import Trisagion.Parsers.Word8 (word32Le)
-import Trisagion.Serializer (Serializer, embed)
+import Trisagion.Serializer (Serializer)
+import qualified Trisagion.Serializers.Binary as Binary (word32Le)
 
 
 {- | The magic t'Sectype' numeric id type. -}
@@ -48,7 +48,7 @@ decodeSectype = capture . fmap Sectype $ word32Le
 
 {- | Encode a t'Sectype' into a 'Word32'. -}
 encodeSectype :: Binary m => Serializer m Sectype
-encodeSectype = contramap unwrap $ embed Binary.word32Le
+encodeSectype = contramap unwrap Binary.word32Le
     where
         unwrap :: Sectype -> Word32
         unwrap (Sectype n) = n
