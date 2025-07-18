fbf0af625c16844069be1ccf1fe0c32f69fc3c90
lambda-dom <grodrigues.math@gmail.com>
2025-07-18 18:49:43 +0100

    `Text` IO read.

--- src/Eeep/IO.hs
+++ src/Eeep/IO.hs
@@ -10,6 +10,7 @@ module Eeep.IO (
 
     -- * 'IO' reads.
     readBinary,
+    readText,
 
     -- * 'IO' writes.
     writeBinary,
@@ -20,11 +21,13 @@ module Eeep.IO (
 import Data.ByteString (ByteString)
 import Data.ByteString.Builder (Builder)
 import qualified Data.ByteString.Builder as Builder (writeFile)
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8Lenient)
 import System.OsPath (OsPath, encodeUtf, decodeUtf)
 import System.File.OsPath (readFile')
 
 
-{- | Encode a 'String' as an utf8 'OsPath'. -}
+{- | Encode a 'String' as an utf-8 'OsPath'. -}
 makePath :: String -> IO OsPath
 makePath = encodeUtf
 
@@ -32,6 +35,14 @@ makePath = encodeUtf
 {- | Read the entire contents of the binary file into memory. -}
 readBinary :: OsPath -> IO ByteString
 readBinary = readFile'
+
+
+{- | Read the entire contents of the utf-8 encoded text file into memory.
+
+Invalid utf-8 characters are replaced with @'\xfffd'@.
+-}
+readText :: OsPath -> IO Text
+readText path = decodeUtf8Lenient <$> readBinary path
 
 
 {- | Write a 'ByteString' 'Builder' to a file. -}
