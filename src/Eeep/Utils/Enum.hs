1b01d255db04560377d89d239ad2a5c4d796ded2
lambda-dom <grodrigues.math@gmail.com>
2026-07-03 19:03:27 +0100

    Consolidate the `Duration` type.

--- src/Eeep/Utils/Enum.hs
+++ src/Eeep/Utils/Enum.hs
@@ -5,21 +5,45 @@ Utilities for enumeration types.
 -}
 
 module Eeep.Utils.Enum (
+    -- * Prisms.
+    enum,
     -- * 'Enum' utilities.
     eitherEnum,
 ) where
 
 -- Imports.
+-- Libraries.
+import Optics.Core (Prism', prism', preview)
+
 -- non-Hackage libraries.
 import Trisagion.Utils.Either ((:+:))
 
 
+{- | Prism for converting between integral types and enumerations.
+
+note(s):
+
+    * If there are more values in the enumeration than the integral type holds, than @review 'enum'@
+    truncates the result to fit the integral type and 'enum' violates the prism laws. It is the
+    responsability of the caller to make sure that that cannot happen.
+-}
+{-# INLINE enum #-}
+enum :: forall a b . (Integral a, Enum b, Bounded b) => Prism' a b
+enum = prism' construct match
+    where
+        construct :: b -> a
+        construct = fromIntegral . fromEnum
+
+        match :: a -> Maybe b
+        match n =
+            if fromEnum (minBound @b) <= m && m <= fromEnum (maxBound @b)
+                then Just $ toEnum m
+                else Nothing
+            where
+                m = fromIntegral n
+
+
 {- | Smart validated constructor for bounded 'Enum' types.-}
 {-# INLINE eitherEnum #-}
-eitherEnum :: forall a b e . (Integral a, Enum b, Bounded b) => e -> a -> e :+: b
-eitherEnum e n =
-    if fromEnum (minBound @b) <= m && m <= fromEnum (maxBound @b)
-        then Right $ toEnum m
-        else Left  $ e
-    where
-        m = fromIntegral n
+eitherEnum :: (Integral a, Enum b, Bounded b) => e -> a -> e :+: b
+eitherEnum e n = maybe (Left e) Right $ preview enum n
